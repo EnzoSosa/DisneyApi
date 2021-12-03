619dbbfa83112dd7d2434faa2590536cb6452cab
EnzoSosa <ensososa4567@gmail.com>
2021-12-03 00:23:43 -0300

    creacion de controler usuario

--- src/main/java/com/example/ChallengeDisney/controller/UserNameController.java
+++ src/main/java/com/example/ChallengeDisney/controller/UserNameController.java
@@ -1,11 +1,48 @@
-
-
 package com.example.ChallengeDisney.controller;
+
+import com.example.ChallengeDisney.entity.UserName;
+import com.example.ChallengeDisney.service.UserNameService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 /**
  *
  * @author Enzo sosa
  */
+@RestController
+@RequestMapping("/auth")
 public class UserNameController {
+
+    @Autowired
+    private UserNameService us;
+
+    @GetMapping("/login")
+    public String login(Model model,
+            @RequestParam(required = false) String userName,
+            @RequestParam(required = false) String error, RedirectAttributes redirectAttributes) {
+
+        try {
+            model.addAttribute("userName", userName);
+        } catch (Exception e) {
+            redirectAttributes.addFlashAttribute("error", e.getMessage());
+            return "redirect:/";
+        }
+
+        return "redirect:/";
+    }
+
+    @PostMapping("/register")
+    public UserName register(@RequestBody UserName userName, MultipartFile image) {
+        return us.createUser(userName, image);
+
+    }
 
 }
--- src/main/java/com/example/ChallengeDisney/service/UserNameService.java
+++ src/main/java/com/example/ChallengeDisney/service/UserNameService.java
@@ -16,55 +16,55 @@ import java.util.Optional;
  */
 @Service
 public class UserNameService {
-    
+
     @Autowired
     private UsernameRepository ur;
-    
+
     @Autowired
     private PictureService ps;
-    
+
     @Transactional
     public UserName createUser(UserName u, MultipartFile image) {
-        
+
         Picture p = ps.save(image);
         u.setPicture(p);
-        
+
         return ur.save(u);
-        
+
     }
-    
+
     @Transactional
     public void upgradeUser(UserName u, MultipartFile image) {
-        
+
         UserName us = ur.findById(u.getUser_Id()).get();
-        
+
         if (us != null) {
-            
+
             u = us;
-            
+
             createUser(u, image);
-            
+
         }
-        
+
     }
-    
+
     @Transactional
     public List<UserName> listAllUser() {
-        
+
         return ur.findAll();
-        
+
     }
-    
+
     @Transactional
     public void deleteUser(String id) {
-        
+
         Optional<UserName> uop = ur.findById(id);
-        
+
         if (uop.isPresent()) {
-            
+
             ur.delete(uop.get());
-            
+
         }
     }
-    
+
 }
